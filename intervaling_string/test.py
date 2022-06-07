8bd5c17f774bda89d3e887f4def367b0360df11f
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2022-06-07 09:24:32 +0430

    code refactor

--- intervaling_string/test.py
+++ intervaling_string/test.py
@@ -15,37 +15,28 @@ class Solution:
         self.dp = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
         
         # base condition
-        for s1_idx in range(len(self.dp)):
-            if s1_idx == 0:
-                self.set_dp_cell(s1_idx, 0, True)
-            else:
-                self.set_dp_cell(s1_idx, 0, 
-                                 self.get_dp_cell(s1_idx - 1, 0) 
-                                 and s1[s1_idx - 1] == s3[s1_idx - 1])
-        
-        for s2_idx in range(len(self.dp[0])):
-            if s2_idx == 0:
-                self.set_dp_cell(0, s2_idx, True)
-            else:
-                self.set_dp_cell(0, s2_idx,
-                                 self.get_dp_cell(0, s2_idx - 1)
-                                 and s2[s2_idx - 1] == s3[s2_idx - 1])
-        
-        for s1_idx in range(1, len(self.dp)):
-            for s2_idx in range(1, len(self.dp[0])):
+        self.set_dp_cell(0, 0, True)
+        for s1_idx in range(0, len(self.dp)):
+            for s2_idx in range(0, len(self.dp[0])):
                 s3_idx = Solution._s3_idx(s1_idx, s2_idx)
+                conditions = []
+                if s1_idx > 0:
+                    up_cond = self.get_dp_cell(s1_idx - 1, s2_idx) and s3[s3_idx - 1] == s1[s1_idx - 1]
+                    conditions.append(up_cond)
+                    
+                if s2_idx > 0:
+                    left_cond = self.get_dp_cell(s1_idx, s2_idx - 1) and s3[s3_idx - 1] == s2[s2_idx - 1]
+                    conditions.append(left_cond)
                 
-                up_cond = self.get_dp_cell(s1_idx - 1, s2_idx) and s3[s3_idx - 1] == s1[s1_idx - 1]
-                left_cond = self.get_dp_cell(s1_idx, s2_idx - 1) and s3[s3_idx - 1] == s2[s2_idx - 1]
-                
-                self.set_dp_cell(s1_idx, s2_idx,
-                                 up_cond
-                                 or left_cond)
+                if len(conditions) == 1:
+                    self.set_dp_cell(s1_idx, s2_idx, conditions[0])
+                elif len(conditions) == 2:
+                    self.set_dp_cell(s1_idx, s2_idx, conditions[0] or conditions[1])
                 
         # from pprint import pprint
         # pprint(self.dp, indent=4, width=80, depth=None)
         
-        return self.get_dp_cell(len(self.dp) - 1, len(self.dp[0]) - 1)
+        return self.get_dp_cell(-1, -1)
 
 
 s = Solution()
