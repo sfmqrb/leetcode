fc09b9010c61a463030da45a4a4601aafd4bc292
Sajad F. Maghrebi <sajad.faghfoor@gmail.com>
2023-04-14 00:52:01 +0330

    feat: reverse integer golang

---
+++ reverse-integer/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverse(x int) int {
+	val := 0
+	for (x != 0) {
+		val *= 10 
+		val += x % 10
+		x /= 10
+	}
+
+	if (1<<31 < val) || (-1<<31 > val) {
+		return 0
+	}
+
+	return val
+}
+
+func main() {
+	x := -123
+	fmt.Println(reverse(x))
+}
